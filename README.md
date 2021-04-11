8f056657891b8b5e48177495f0c135fffe4cbc72
TURKCELL\TTODURMAZ <ozer.durmaz@turkcell.com.tr>
2021-04-11 12:15:06 +0300

    mongodb added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-ejs-blog
+
+set UserName and Password in app.js for mongodb connection (mongodb atlas is used in development)
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,14 +17,34 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+mongoose.connect("mongodb+srv://<user>:<Password>@cluster0.zbbtr.mongodb.net/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const posts = [];
+const postSchema = {
+
+  title: String,
+
+  content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
 
 app.get("/", function(req, res) {
-  res.render('home', {
-    content: homeStartingContent,
-    posts: posts,
-    lodash : _
+  Post.find({}, function(err, foundPosts) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      res.render('home', {
+        content: homeStartingContent,
+        posts: foundPosts
+      });
+    }
   });
 });
 
@@ -46,26 +66,33 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:title", function(req, res) {
-  //kebab case a-b-c
-  const title = _.lowerCase(req.params.title);
-  const post = posts.find((p) => _.lowerCase(p.title)  === title);
-  if (post) {
-    res.render('post', {
-      post: post
-    });
-  }
-  else{
-    res.redirect("/");
-  }
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({
+    _id: requestedPostId
+  }, function(err, foundPost) {
+    if (!err) {
+      if (!foundPost) {
+        res.redirect("/");
+      } else {
+        res.render('post', {
+          post: foundPost
+        });
+      }
+    }
+  });
 });
 
 app.listen(3000, function() {
